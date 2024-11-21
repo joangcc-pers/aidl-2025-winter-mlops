bf946d959036fe96029add3e0f74419978b224d6
joangccantero <joan.gonzalez-condecantero@vistaprint.com>
2024-11-21 10:41:51 +0100

    :sparkles: session-3 Develop code for model and main

--- session-3/main.py
+++ session-3/main.py
@@ -17,8 +17,21 @@ def train_single_epoch(model, train_loader, optimizer):
     model.train()
     accs, losses = [], []
     for x, y in train_loader:
-        # You will need to do y = y.unsqueeze(1).float() to add an output dimension to the labels and cast to the correct type
-        ...
+
+        # You will need to do y = y.unsqueeze(1).float() 
+        # to add an output dimension to the labels and cast to the correct type
+        #-esteve
+        x, y = x.to(device), y.to(device)
+        y = y.unsqueeze(1).float()
+        optimizer.zero_grad()
+        output = model(x)
+        # loss = F.binary_cross_entropy(output, y)
+        loss = F.binary_cross_entropy_with_logits(output,y)
+        # print(output)
+        loss.backward()
+        optimizer.step()
+        acc = binary_accuracy(output, y)
+
         losses.append(loss.item())
         accs.append(acc.item())
     return np.mean(losses), np.mean(accs)
@@ -29,7 +42,14 @@ def eval_single_epoch(model, val_loader):
     with torch.no_grad():
         model.eval()
         for x, y in val_loader:
-            ...
+            #-esteve
+            x, y = x.to(device), y.to(device)
+            y = y.unsqueeze(1).float()
+            output = model(x)
+            # loss = F.binary_cross_entropy(output, y)
+            loss = F.binary_cross_entropy_with_logits(output,y)
+            acc = binary_accuracy(output, y)
+
             losses.append(loss.item())
             accs.append(acc.item())
     return np.mean(losses), np.mean(accs)
@@ -37,10 +57,18 @@ def eval_single_epoch(model, val_loader):
 
 def train_model(config):
 
-    data_transforms = transforms.Compose([...])
-    train_dataset = ImageFolder...
+    # data_transforms = transforms.Compose([...])
+    data_transforms = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+    #train_dataset = ImageFolder...
+    train_dataset = ImageFolder('dataset/cars_vs_flowers/training_set', transform=data_transforms)
     train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
-    test_dataset = ImageFolder...
+    
+    #test_dataset = ImageFolder...
+    test_dataset = ImageFolder('dataset/cars_vs_flowers/test_set', transform=data_transforms)
     test_loader = DataLoader(test_dataset, batch_size=config["batch_size"])
 
     my_model = MyModel().to(device)
@@ -60,8 +88,6 @@ if __name__ == "__main__":
     config = {
         "lr": 1e-3,
         "batch_size": 64,
-        "epochs": 5,
+        "epochs": 2,
     }
     my_model = train_model(config)
-
-    
--- session-3/model.py
+++ session-3/model.py
@@ -4,9 +4,34 @@ import torch.nn.functional as F
 
 class MyModel(nn.Module):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        ...
+        self.conv_layers = nn.Sequential(
 
-    def forward(self, x):
-        ...
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.5), # Elimina certes unitats q tenen algun valor o zeros passos
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout(0.5),
+        )
+
+        self.mlp = nn.Sequential(
+            nn.Linear(64 * 32 * 32, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+    def forward(self,
+        x: torch.Tensor) -> torch.Tensor:
+        x = self.conv_layers(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.mlp(x)
+        return x
